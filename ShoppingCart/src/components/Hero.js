d903c2dc5a46c60b482e5d2899a2f9e0360cf0bb
Neha Sontakke <wasnik.neha@gmail.com>
2022-07-13 18:10:36 +0530

    added the remove funtion

--- ShoppingCart/src/components/Hero.js
+++ ShoppingCart/src/components/Hero.js
@@ -28,7 +28,18 @@ const Hero =()=> {
     ]);
     };
    const removeItem = (id) => {
+    const newItems = cartItems.filter(cartItem => cartItem.id !== id);
+    setCartItems(newItems);
 
+    const itemCountResult = newItems.reduce(
+        (acc,val) => (acc +=  val.cartCount),0
+    );
+    setItemCount(itemCountResult);
+
+    const priceResult =  newItems.reduce(
+    (acc,val) => (acc += val.price * val.cartCount),0
+    );
+    setItemCount(priceResult);
    }
 
     return(
